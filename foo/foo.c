c78beccd91a773253534d3785954e09d99b30613
C.D.Luminate <cdluminate@gmail.com>
2014-11-07 05:23:56 +0000

    foo: read() md_ans from file; add md5bin.c

--- foo/foo.c
+++ foo/foo.c
@@ -6,6 +6,10 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
 #include <openssl/md5.h>
 #include <omp.h>
 
@@ -13,10 +17,23 @@ int
 main (int argc, char **argv)
 {
 	char md_ans[16];
-	bzero (md_ans, 16);
+	//bzero (md_ans, 16);
+	//MD5 ((const unsigned char *)argv[1], strlen(argv[1]), (unsigned char *)md_ans);
+	
+	/*char md_ans[16] = {  this is md5 of '0000' 
+			   0x4a, 0x7d, 0x1e, 0xd4,
+			   0x14, 0x47, 0x4e, 0x40,
+			   0x33, 0xac, 0x29, 0xcc,
+			   0xb8, 0x65, 0x3d, 0x9b }; */
 
+	int fd = open ("aaaa.md5", O_RDONLY);
+	if (fd == -1) {
+		perror ("open");
+		exit (1);
+	}
+	read (fd, md_ans, 16);
+	
 	//char md[16];
-
 	//char c[4];
 	//bzero (c, 4);
 
@@ -25,13 +42,12 @@ main (int argc, char **argv)
 	char c2 = 0;
 	char c3 = 0;
 
-	if (argc != 2)
-		exit (-1);
+	//if (argc != 2)
+	//	exit (-1);
 
-	MD5 ((const unsigned char *)argv[1], strlen(argv[1]), (unsigned char *)md_ans);
 
 	//#pragma omp parallel for num_threads(4) shared(c1,c2,c3) private(x,y,z,w)  
-	#pragma omp parallel for num_threads(4) private(c1,c2,c3)
+	//#pragma omp parallel for num_threads(4) private(c1,c2,c3)
 	for (c0 = 32; c0 < 127; c0++) {
 		char c[5];
 		bzero (c, 5);
@@ -51,15 +67,15 @@ main (int argc, char **argv)
 
 					MD5 ((const unsigned char *)c, 4, (unsigned char *)md);
 					if (memcmp (md, md_ans, 16) == 0) {
-						printf ("%c%c%c%c           \n", c0, c1, c2, c3);
+						//printf ("%c%c%c%c           \n", c0, c1, c2, c3);
 						write (2, c, 4);
+						write (2, "\n", 2);
 						exit (0);
 					}
 				}
 			}
 		}
 	}
-	
 
 	return 0;
 }
---
+++ foo/md5bin.c
@@ -0,0 +1,28 @@
+/* md5bin.c
+   cdluminate@163.com
+   MIT licence
+ */
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <openssl/md5.h>
+
+int
+main (int argc, char **argv)
+{
+/*
+           unsigned char *MD5(const unsigned char *d, unsigned long n,
+	                            unsigned char *md);
+ */
+	char md[16];
+	
+	if (argc != 2)
+		exit (1);
+
+	MD5 ((unsigned char *)argv[1], strlen(argv[1]), (unsigned char *)md);
+
+	write (1, md, 16);
+
+	return 0;
+}
