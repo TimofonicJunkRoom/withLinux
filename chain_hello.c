36af5a5a759d745f3ea2aae91584990d5f64f888
Zhou Mo <cdluminate@gmail.com>
2015-07-14 02:46:56 +0000

    add chain demo chain_hello.c

---
+++ chain_hello.c
@@ -0,0 +1,15 @@
+#include "chain.c"
+
+int
+main (void)
+{
+	struct CHAIN * root = NULL;
+	struct CHAIN * cp   = NULL;
+	root = chain_init ("HEAD", NULL);
+	chain_print (root);
+	chain_print (cp);
+	cp = chain_fastappend (root, "Laplace", NULL);
+	chain_print (cp);
+	return 0;
+}
+	
