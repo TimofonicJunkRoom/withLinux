1b5d7137ac25b33f4cb8a839c55bb7b76b01bbac
Zhou Mo <cdluminate@gmail.com>
2015-07-14 03:47:25 +0000

    Update demo, fix chainpp.c

--- chain_hello.c
+++ chain_hello.c
@@ -1,26 +1,36 @@
 #include "chain.c"
+#include "chainpp.c"
 
 int
 main (void)
 {
+	/* init chainpp extension */
+	chainpp_init (&chainpp);
+
 	struct CHAIN * root = NULL;
 	struct CHAIN * cp   = NULL;
+
+	/* print NULL */
+	chain_print (cp);
+
+	/* create chain */
 	root = chain_init ("HEAD", NULL);
 	chain_print (root);
-	chain_print (cp);
+	printf ("### root node created \n");
+	
+	/* adding tail */
 	cp = chain_fastappend (root, "Laplace", NULL);
-	chain_print (root);
-	chain_print (cp);
-
-	printf ("chain_dump ()\n");
-	chain_dump (cp);
-
 	chain_fastappend (cp, "Gauss", NULL);
 	chain_fastappend (cp, "Fermi", NULL);
+	chainpp.fastappend (root, "Lumin", NULL);
 	chain_fastappend (cp, "Galois", NULL);
 	chain_fastappend (root, "Fourier", NULL);
-	chain_dump (cp);
 
+	/* dump */
+	printf ("############ chainpp.dump ()\n");
+	chainpp.dump (root);
+
+	/* destroy */
 	chain_destroy (root);
 	return 0;
 }
--- chainpp.c
+++ chainpp.c
@@ -1,3 +1,8 @@
+/* libstack
+   2015 Lumin
+   this is a pseudo cpp extension in ANSI C, for libstack
+ */
+
 struct CHAINPP {
 	void           (* print             )(struct CHAIN * node);
 	struct CHAIN * (* _tail             )(struct CHAIN * head);
@@ -10,13 +15,17 @@ struct CHAINPP {
 	void *         (* destroy           )(struct CHAIN * head);
 } chainpp ;
 
-chainpp.print     = chain_print;
-chainpp._tail     = _chain_tail;
-chainpp._head     = _chain_head;
-chainpp.create    = chain_create;
-chainpp.init      = chain_init;
-chainpp.append    = chain_append;
-chainpp.fastappend= chain_fastappend;
-chainpp.dump      = chain_dump;
-chainpp.destroy   = chain_destroy;
-
+void
+chainpp_init (struct CHAINPP * p)
+{
+	p->print      = chain_print;
+	p->_tail      = _chain_tail;
+	p->_head      = _chain_head;
+	p->create     = chain_create;
+	p->init       = chain_init;
+	p->append     = chain_append;
+	p->fastappend = chain_fastappend;
+	p->dump       = chain_dump;
+	p->destroy    = chain_destroy;
+	return;
+}
