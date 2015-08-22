68a12b003a0d7974b44a7ccc3ea227804af34807
Zhou Mo <cdluminate@gmail.com>
2015-08-22 09:50:02 +0000

    update foolinterp.m

--- interp/foolinterp.m
+++ interp/foolinterp.m
@@ -10,8 +10,8 @@ K=101;
 output = zeros([I,J,K]);
 
 % round 1
-for x = 1:10:I-1
-	for y = 1:10:J-1
+for x = 1:10:I
+	for y = 1:10:J
 		for z = 1:K-1
 			%x %y %z
 			ax = floor((x-1)/10)+1;
@@ -25,8 +25,8 @@ for x = 1:10:I-1
 end
 
 % round 2
-for x = 1:10:I-1
-	for z = 1:K-1
+for x = 1:10:I
+	for z = 1:K
 		for y = 1:J-1
 			ax = floor((x-1)/10)+1;
 			ay = floor((y-1)/10)+1;
@@ -40,8 +40,8 @@ end
 
 % round 3
 for x = 1:I-1
-	for z = 1:K-1
-		for y = 1:J-1
+	for z = 1:K
+		for y = 1:J
 			ax = floor((x-1)/10)+1;
 			ay = floor((y-1)/10)+1;
 			az = floor((z-1)/10)+1;
@@ -51,3 +51,5 @@ for x = 1:I-1
 		end
 	end
 end
+
+output (I,J,K) = input(OI,OJ,OK);
