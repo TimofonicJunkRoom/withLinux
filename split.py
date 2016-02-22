da2631c962078762005106a3c2431e965d772428
Lumin Zhou <CDLuminate@users.noreply.github.com>
2016-02-22 05:51:14 +0000

    add split.py

---
+++ split.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+
+import json
+import subprocess
+import sys
+import os
+
+debug = 1
+
+def main ():
+    if len(sys.argv) != 2:
+        print ("missing argv[1]")
+
+    if debug: print (sys.argv)
+
+    with open(sys.argv[1], 'r') as f:
+        content = f.read()
+        d = json.loads(content)
+        if debug: print (d.keys())
+        '''
+        ['images', 'licenses', 'annotations', 'info']
+        '''
+        if debug: print (d["images"][0])
+        middle = int(len(d['images'])/2)
+        print ("total", len(d['images']), "middle", middle)
+
+        left = {}
+        left['licenses'] = d['licenses']
+        left['annotations'] = d['annotations']
+        left['info'] = d['info']
+        left['images'] = d['images'][0:middle]
+        with open(sys.argv[1] + ".left", "w+") as fl:
+            fl.write(json.dumps(left))
+            print ("split: part 1", sys.argv[1]+".left")
+
+        right = {}
+        right['licenses'] = d['licenses']
+        right['annotations'] = d['annotations']
+        right['info'] = d['info']
+        right['images'] = d['images'][middle:]
+        with open(sys.argv[1] + ".right", "w+") as fr:
+            fr.write(json.dumps(right))
+            print ("split: part 2", sys.argv[1]+".right")
+
+main ()
