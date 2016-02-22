e9d676a0c106548534f2cd90859bf1b41e9c6cec
Lumin Zhou <CDLuminate@users.noreply.github.com>
2016-02-22 13:43:21 +0000

    add check_jpeg.py, JPEG integrity checker

---
+++ check_jpeg.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+# reference: https://github.com/cdluminate/withlinux
+
+import glob
+
+jpeglist = glob.glob('pool/*.jpg')
+
+count = 0
+for jpg in jpeglist:
+    with open(jpg, 'rb') as f:
+        pic = f.read()
+    if len(pic) > 0:
+        if pic[0]!=255 or pic[1]!=216: # SOI
+            count = count + 1
+            print ("%07d"%count, "NON-JPEG:", jpg, "(%s,%s)"%(pic[0], pic[1]))
+        else:
+            if pic[-2]!=255 or pic[-1]!= 217: # EOI
+                count = count + 1
+                print ("%07d"%count, "EOI-MISSING:", jpg, "(%s,%s)"%(pic[-2], pic[-1]))
