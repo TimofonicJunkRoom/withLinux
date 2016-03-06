fdfc4f0663ab86ae884d323e7ae07c06ae2a173d
Zhou Mo <cdluminate@gmail.com>
2016-03-06 04:55:10 +0000

    extra: update lib/demo.c

--- lib/demo.c
+++ lib/demo.c
@@ -1,4 +1,18 @@
+#include <unistd.h>
 #include "cdalog.h"
+
+void
+train (void)
+{
+	int i = 0;
+	while (i <= 3) {
+		LOG_INFOF ("iteration %d\n", i);
+		i++;
+		LOG_DEBUG ("sleeping for 1 sec\n");
+		sleep (1);
+	}
+	return;
+}
 
 int
 main (void)
@@ -7,5 +21,9 @@ main (void)
 	LOG_WARN ("example of warning\n");
 	LOG_ERROR ("example of error\n");
 	LOG_DEBUG ("example of debug information\n");
+	
+	train ();
+
+	LOG_INFOF ("%s\n", "Happy hacking!");
 	return 0;
 }
