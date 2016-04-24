369744d12ff3ead531d1af4c7f4f51f3ad1b8846
Zhou Mo <cdluminate@gmail.com>
2016-04-24 02:53:04 +0000

    ai:torch: add example axeqb.lua

---
+++ ai/torch/axeqb.lua
@@ -0,0 +1,57 @@
+require 'torch'
+require 'nn'
+local log = require 'lumin_log.lua'
+
+log.warn('Solve Ax=b matrix equation')
+local lr = 0.1 --> learning rate
+
+log.info('data')
+
+local A = torch.Tensor():eye(5,5)
+local b = torch.Tensor():ones(5,1) * 3
+
+print(A)
+print(b)
+
+log.info('net')
+--local x = nn.Sequential():add(nn.Linear(5, 1, false))
+local x = nn.Linear(5, 1, false)
+
+log.info('loss:MSE')
+local crit = nn.MSECriterion()
+--local crit = nn.AbsCriterion() -- NOTE: not easy to converge
+
+log.info('solve')
+local loss_initial = 0
+for i = 1, math.huge do
+   log.info('iter '..tostring(i))
+
+   --forward
+   local netout = x:forward(A)
+   print(netout)
+   local loss = crit:forward(netout, b)
+   print(loss)
+   if loss_initial == 0 then loss_initial = loss end
+
+   --backward
+   x:zeroGradParameters()
+   local critback = crit:backward(netout, b)
+   x:backward(A, critback)
+
+   --update
+   x:updateParameters(lr)
+
+   --detection
+   --if loss < 1e-7 then break end
+   if loss < 1e-9 then break end
+   if loss > 20 * loss_initial then
+      log.error('Loss explosion, initial='
+        ..tostring(loss_initial)..' Current='..tostring(loss))
+      os.exit(1)
+   end
+end
+
+log.info('converged, dump solution')
+--print(x:getParameters())
+assert(x.bias == nil)
+print(x.weight)
