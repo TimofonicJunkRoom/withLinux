20c4915405015fccaa14fc2d426eabf3e2799693
Zhou Mo <cdluminate@gmail.com>
2016-06-27 02:55:26 +0000

    lang:py3: update spider.py

--- lang/py3/spider/spider.py
+++ lang/py3/spider/spider.py
@@ -10,22 +10,28 @@ automatic image downloader
 
 TODO:
 
-* fix filename extension with python3-magic
-*  upgrade and use webdriver
+* upgrade and use webdriver
 '''
 import urllib.parse
 import urllib3
 import re
 import os
+import magic
 
+# configure
 url_template="http://image.baidu.com/search/index" + \
   "?tn=baiduimage&ipn=r&cl=2&lm=-1&st=-1&fm=result&fr=&sf=1" + \
   "&pv=&ic=0&nc=1&z=&se=1&showtab=0&fb=0&width=&height=&face=0" + \
   "&istype=2&ie=utf-8&word="
 keyword=u'暴走表情'
+
+# initialization
 re_pic = re.compile('"objURL":"(.*?)",')
 pm = urllib3.PoolManager()
+ms = magic.open(magic.NONE)
+ms.load()
 
+# helpers
 def download(url):
   if not os.path.exists('pool'):
     print('create pool directory')
@@ -33,7 +39,8 @@ def download(url):
   pic = pm.request('GET', url)
   basename = os.path.basename(urllib.parse.unquote(url))
   path = 'pool/' + basename
-  print('write file', path)
+  filemagic = ms.buffer(pic.data).split(',')[0]
+  print('write', filemagic, '->', path)
   #print(pic.data)
   with open(path, 'wb') as f:
     f.write(pic.data)
@@ -51,7 +58,8 @@ def main():
   #print(pic_urls)
   print(len(pic_urls), 'images parsed')
   print('starting to download')
-  res = list(map(download, pic_urls))
+  #res = list(map(download, pic_urls)) # we don't download all, this is only a demo
+  res = list(map(download, pic_urls[0:5]))
   print(res)
 
 main()
