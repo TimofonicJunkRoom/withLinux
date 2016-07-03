9c597fef0385c53c606d7e327bf336d0ff70c1b7
Zhou Mo <cdluminate@gmail.com>
2016-07-03 14:35:55 +0000

    lang:sh: aptsource.sh update

--- lang/sh/aptsource.sh
+++ lang/sh/aptsource.sh
@@ -16,12 +16,32 @@ dialog --stdout --no-lines --no-shadow \
   --clear "$@" \
   --radiolist "Select a mirror:" \
     16 72 0 \
-    "ftp.cn.debian.org" "" on \
-    "mirror.tuna.tsinghua.edu.cn" "" off \
-    "ftp.xdlinux.info" "" off \
+    "http://ftp.cn.debian.org" "" on \
+    "http://mirror.tuna.tsinghua.edu.cn" "" off \
+    "http://ftp.xdlinux.info" "" off \
+    "file:///media/lumin/Seagate/DebArchive.git/jessie/" "" off \
   2>&1 1>$tempfile 
 retval=$?
 mirror=$(cat $tempfile)
 echo "select mirror $mirror"
+
+# --[ select release with radiolist
+dialog --stdout --no-lines --no-shadow \
+  --title "Select Release" "$@" \
+  --checklist "Select a release:" \
+    16 72 10 \
+    "jessie" "" on \
+    "jessie-backports" "" on \
+    "jessie-proposed-updates" "" on \
+    "jessie/updates" "" on \
+    "sid" "" off \
+    "experimental" "" off \
+    "corresponding-source" "" off \
+  2>&1 1>$tempfile 
+retval=$?
+releases=$(cat $tempfile)
+echo "select release $releases"
+
+#deb http://debug.mirrors.debian.org/debian-debug unstable-debug main
 
 #EOF
