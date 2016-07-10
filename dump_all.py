55db618725a967676e9b7cc6f8c9a073a0787835
Zhou Mo <cdluminate@gmail.com>
2016-07-10 14:18:40 +0000

    optimize dump_all.py performance

--- dump_all.py
+++ dump_all.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 import json
+import os
 j = json.loads(open('annotations/captions_train2014.json').read())
 jv = json.loads(open('annotations/captions_val2014.json').read())
 
@@ -22,23 +23,30 @@ idlall = sorted(idl + idlv)
 print('idlall', type(idlall), len(idlall))
 
 d = {}
-counter=0
-for eachid in idlall:
-  if counter > 32:
-    print('stage1: progress %.2f%%'%((100*eachid)/idlall[-1]))
-    counter = 0
-  sents = []
-  for eachsent in annotations:
-    if eachsent['image_id'] == eachid:
-      sents.append(eachsent['caption'])
-    else:
-      pass
-  d[eachid] = sents
-  counter = counter + 1
-
+pcounter=0
+rcounter=1
+for eachsent in annotationsall:
+  if pcounter > 30000:
+    print('stage1: progress %.1f%%'%((100*rcounter)/len(annotationsall)))
+    pcounter = 0
+  cur_image_id = eachsent['image_id']
+  if not cur_image_id in d:
+    d[cur_image_id] = []
+  d[cur_image_id].append(eachsent['caption'])
+  pcounter = pcounter + 1
+  rcounter = rcounter + 1
+  
 print('stage2: writing text file')
 with open('coco_all_sents.txt', 'w+') as f:
   for eachkey in d.keys():
     for eachsent in d[eachkey]:
       #print ('%s: %s'%(eachkey, eachsent))
-      f.write('%s: %s\n'%(eachkey, eachsent))
+      f.write('%s: %s\n'%(eachkey, eachsent.strip()))
+
+print('validation: out-of-order raw sents')
+with open('coco_all_sents.val.txt', 'w+') as f:
+  for eachsent in annotationsall:
+    f.write('%s: %s\n'%(eachsent['image_id'], eachsent['caption'].strip()))
+
+os.system('''nl coco_all_sents.txt | tail -n1 ''')
+os.system('''nl coco_all_sents.val.txt | tail -n1 ''')
