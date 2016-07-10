43a19441e21e88f01e3f80bd905c1da76802c2eb
Zhou Mo <cdluminate@gmail.com>
2016-07-10 15:25:51 +0000

    correct dump_all.py

--- dump_all.py
+++ dump_all.py
@@ -40,22 +40,65 @@ print('stage2: writing text file')
 with open('coco_all_sents.txt', 'w+') as f:
   for eachkey in d.keys():
     for eachsent in d[eachkey]:
-      #print ('%s: %s'%(eachkey, eachsent))
-      f.write('%s: %s\n'%(eachkey, eachsent.strip()))
-
-print('stage3: validation: out-of-order raw sents')
-with open('coco_all_sents.val.txt', 'w+') as f:
-  for eachsent in annotationsall:
-    f.write('%s: %s\n'%(eachsent['image_id'], eachsent['caption'].strip()))
+      eachsent = eachsent.strip().replace('\n', ' ').strip()
+      if len(eachsent.strip()) == 0:
+        continue
+      if eachsent[-1] != '.':
+        eachsent = eachsent + '.'
+      f.write('%s: %s\n'%(eachkey, eachsent))
 
 os.system('''nl coco_all_sents.txt | tail -n1 ''')
-os.system('''nl coco_all_sents.val.txt | tail -n1 ''')
 
-print('stage4: strip image_num')
+print('stage3: strip coco_all_sents.txt')
+with open('coco_all_sents-n.confirm.txt', 'w+') as fo:
+  with open('coco_all_sents.txt', 'r') as fi:
+    lines = fi.readlines()
+    for (k,line) in enumerate(lines):
+      # tester
+      line = line.encode().strip()
+      try:
+        needle = line[-1]
+      except IndexError:
+        print('TROUBLEMAKER:', line, len(line))
+        del line
+        continue
+      # write
+      if len(line.strip()) == 0:
+        continue
+      if len(bytes(line.strip()))==0:
+        continue
+      else:
+        n = fo.write('%s\n'%line)
+        assert(n>0)
+
+os.system('''nl coco_all_sents.txt | tail -n1 ''')
+
+print('stage4: performing check against coco_all_sents.txt')
+with open('coco_all_sents.txt', 'r') as f:
+  lines = f.readlines()
+  assert(len(lines) == 616767)
+  for (k,line) in enumerate(lines):
+    line = line.strip()
+    if len(line) == 0:
+      print('warning:', k, '-th line empty')
+  for (k,line) in enumerate(lines):
+    if line.strip()[-1] != '.':
+      print('warning:', k, '-th line missing period:', line.strip()[-1])
+  for (k, line) in enumerate(lines):
+    try:
+      needle = line[-1]
+    except IndexError:
+      print('trouble line not removed!')
+
+os.system('''nl coco_all_sents-n.confirm.txt | tail -n1 ''')
+
+print('stage5: strip image_num')
 with open('coco_all_sents.txt', 'r') as fi:
   with open('coco_all_sents-n.txt', 'w+') as fo:
     lines = fi.readlines()
     for line in lines:
+      line = line.strip() # ensure the last char is . or *^space
+
       line = line.strip().split(' ')[1:] # remove heading image_num
       fo.write('%s\n'%(' '.join(line)))
 
