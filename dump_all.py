d0b1a3b5365f881b186b2928c32560fa0074784d
Zhou Mo <cdluminate@gmail.com>
2016-07-10 12:45:10 +0000

    update dump_all.py

--- dump_all.py
+++ dump_all.py
@@ -1,24 +1,42 @@
 #!/usr/bin/python3
 import json
 j = json.loads(open('annotations/captions_train2014.json').read())
+jv = json.loads(open('annotations/captions_val2014.json').read())
 
 annotations = j['annotations']
+annotationsv = jv['annotations']
+annotationsall = annotations + annotationsv
+print('annotationsall', type(annotationsall), 'length', len(annotationsall))
 
 imagel = [j['images'][i] for i in range(len(j['images']))]
-
 idl = [ imagel[i]['id'] for i in range(len(imagel)) ]
-idl = sorted(idl)
-print ('sorted list len ', len(idl))
+assert(len(idl) == len(imagel))
+print('train image', type(imagel), len(imagel))
+
+imagelv = [jv['images'][i] for i in range(len(jv['images']))]
+idlv = [ imagelv[i]['id'] for i in range(len(imagelv)) ]
+assert(len(idlv) == len(imagelv))
+print('val image', type(imagelv), len(imagelv))
+
+idlall = sorted(idl + idlv)
+print('idlall', type(idlall), len(idlall))
 
 d = {}
-for eachid in idl:
-  print('progress %f%%'%((100*eachid)/idl[-1]))
+counter=0
+for eachid in idlall:
+  if counter > 32:
+    print('stage1: progress %.2f%%'%((100*eachid)/idlall[-1]))
+    counter = 0
   sents = []
   for eachsent in annotations:
     if eachsent['image_id'] == eachid:
       sents.append(eachsent['caption'])
+    else:
+      pass
   d[eachid] = sents
+  counter = counter + 1
 
+print('stage2: writing text file')
 with open('coco_all_sents.txt', 'w+') as f:
   for eachkey in d.keys():
     for eachsent in d[eachkey]:
