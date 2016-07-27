020a745c1986e11becf56347e4569fdd32307862
Zhou Mo <cdluminate@gmail.com>
2016-07-27 13:46:46 +0000

    lang:perl: intro.pl ok

--- lang/perl/intro.pl
+++ lang/perl/intro.pl
@@ -4,8 +4,7 @@ use warnings;
 
 # basic syntax
 print "Perl quick Introduction\n";
-print "Reference: http://perldoc.perl.org/perlintro.html\n";
-print 'in this case \n is not excaped';
+print "Reference: http://perldoc.perl.org/perlintro.html\n"; print 'in this case \n is not excaped';
 print "\n";
 print 42, "\n";
 print ("hello perl, you can omit parentheses.\n");
@@ -78,4 +77,110 @@ my @colors = values %fruit_color; # get values from the hash table
 ## until (condition) { ... }
 ## print "hello" while 1;
 
-print "TODO\n";
+print "For-loop\n";
+my $i;
+for ($i = 0; $i < 10; $i++) {
+  print "$i\n";
+}
+
+foreach (@animals) {
+  print "This is $_\n";
+}
+
+foreach my $key (keys %fruit_color) {
+  print "The value of $key is $fruit_color{$key}\n";
+}
+
+# built in operators and functions
+# eq, ne, lt, gt, le, ge are for string comparison
+# . is for string concatenation
+# x is for string multiplication
+# .. is range operator
+
+# files and I/O
+open(my $in, "<", "input.txt")   or die "missing input.txt: $!";
+open(my $out, ">", "output.txt") or die "cannot open output.txt: $!";
+open(my $log, ">>", "my.log")    or die "cannot open my.log: $!";
+
+my $line = <$in>; # this reads a line
+my @lines = <$in>; # this reads lines into a list
+
+seek($in, 0, 0);
+while (<$in>) { # assign each line to $_
+  print "read: $_";
+}
+
+print STDERR "print to stderr\n";
+print $out "print to out\n";
+print $log "print to log\n";
+
+close $in or die "in: $!";
+close $out or die "out: $!";
+close $log or die "log: $!";
+
+# regular expressions
+print "--[ regular expressions ]--\n";
+my $msg = "hello world";
+if ($msg =~ /hello/) { # simple match
+  print "variable \$msg contains hello\n";
+}
+$msg =~ s/world/perl/; # replace one instance
+print "$msg\n";
+$msg =~ s/l/x/g; # global replace
+print "$msg\n";
+# more complex regular expressions
+#     .                   a single character
+#     \s                  a whitespace character (space, tab, newline, ...)
+#     \S                  non-whitespace character
+#     \d                  a digit (0-9)
+#     \D                  a non-digit
+#     \w                  a word character (a-z, A-Z, 0-9, _)
+#     \W                  a non-word character
+#     [aeiou]             matches a single character in the given set
+#     [^aeiou]            matches a single character outside the given set
+#     (foo|bar|baz)       matches any of the alternatives specified
+#     ^                   start of string
+#     $                   end of string
+#
+#     *                   zero or more of the previous thing
+#     +                   one or more of the previous thing
+#     ?                   zero or one of the previous thing
+#     {3}                 matches exactly 3 of the previous thing
+#     {3,6}               matches between 3 and 6 of the previous thing
+#     {3,}                matches 3 or more of the previous thing
+#
+#     /^\d+/              string starts with one or more digits
+#     /^$/                nothing in the string (start and end are
+#                         adjacent)
+#     /(\d\s){3}/         three digits, each followed by a whitespace
+#                                              character (eg "3 4 5 ")
+#      /(a.)+/             matches a string in which every odd-numbered
+#                                              letter is a (eg "abacadaf")
+#      # This loop reads from STDIN, and prints non-blank lines:
+#      while (<>) {
+#              next if /^$/;
+#         print;
+#     }
+
+my $email = "user\@example.org";
+if ($email =~ /([^@]+)@(.+)/) {
+  print "username $1\n";
+  print "hostname $2\n";
+}
+
+# subroutines
+sub logger {
+  my $logmessage = shift;
+  open my $logfile, ">>", "my.log" or die "Could not open my.log: $!";
+  print $logfile $logmessage;
+}
+
+logger("We have a logger subroutine!");
+
+sub square {
+    my $num = shift;
+    my $result = $num * $num;
+    return $result;
+}
+
+$sq = square(8);
