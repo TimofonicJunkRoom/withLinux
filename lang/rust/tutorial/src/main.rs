a3b4b1c7922870bc4a299e28cc140dfee43149ab
Zhou Mo <cdluminate@gmail.com>
2016-08-06 16:32:39 +0000

    lang:rust: tutorial++

--- lang/rust/tutorial/src/main.rs
+++ lang/rust/tutorial/src/main.rs
@@ -365,6 +365,71 @@ fn syntax () {
     }
   }
   { // 4.16 method syntax
+    struct Circle { x: f64, y: f64, radius: f64, }
+    impl Circle {
+      fn area(&self) -> f64 {
+        std::f64::consts::PI * (self.radius * self.radius)
+      }
+      fn grow(&self, inc: f64) -> Circle {
+        Circle { x: self.x, y: self.y, radius: self.radius + inc }
+      }
+      fn new(x: f64, y: f64, radius: f64) -> Circle { // associated function
+        Circle { x: x, y: y, radius: radius, }
+      }
+    }
+    impl Circle { // impl can be split up
+      fn reference(&self) {
+        println!("taking self by reference!");
+      }
+      fn mutable_reference(&mut self) {
+        println!("taking self by mutable reference!");
+      }
+      fn takes_ownership(self) {
+        println!("taking ownership of self!");
+      }
+    }
+    let c = Circle { x: 0.0, y: 0.0, radius: 2.0 };
+    println!("{}", c.area());
+    let b = c.grow(2.0);
+    println!("{}", b.area());
+    let d = Circle::new(0., 1., 2.);
+    // there is no method overloading or default argument in rust
+    struct CircleBuilder {
+      x: f64,
+      y: f64,
+      radius: f64,
+    }
+    impl CircleBuilder {
+      fn new() -> CircleBuilder {
+        CircleBuilder { x: 0.0, y: 0.0, radius: 1.0, }
+      }
+      fn x(&mut self, coordinate: f64) -> &mut CircleBuilder {
+        self.x = coordinate;
+        self
+      }
+      fn y(&mut self, coordinate: f64) -> &mut CircleBuilder {
+        self.y = coordinate;
+        self
+      }
+      fn radius(&mut self, radius: f64) -> &mut CircleBuilder {
+        self.radius = radius;
+        self
+      }
+      fn finalize(&self) -> Circle {
+        Circle { x: self.x, y: self.y, radius: self.radius }
+      }
+    }
+    let c = CircleBuilder::new()
+                .x(1.0)
+                .y(2.0)
+                .radius(2.0)
+                .finalize();
+    println!("area: {}", c.area());
+    println!("x: {}", c.x);
+    println!("y: {}", c.y);
+  }
+  { // 4.17 strings
+    // FIXME
   }
   info!("syntax() done");
 } // fn syntax()
