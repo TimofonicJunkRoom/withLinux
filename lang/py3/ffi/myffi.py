aec385182cad70a381859fa93b38ed116c350f92
Zhou Mo <cdluminate@gmail.com>
2016-08-07 08:57:23 +0000

    lang:py3: add ffi

---
+++ lang/py3/ffi/myffi.py
@@ -0,0 +1,20 @@
+# https://docs.python.org/3/library/ctypes.html
+# http://starship.python.net/crew/theller/ctypes/tutorial.html
+import ctypes
+
+libc = ctypes.cdll.LoadLibrary("libc.so.6")
+libc = ctypes.CDLL("libc.so.6")
+
+print(libc.getpid())
+
+kernel = ctypes.CDLL("./kernel.so")
+print(kernel.kernel()) # output of the kernel function is discard
+
+import cffi
+ffi = cffi.FFI()
+ffi.cdef("""
+    int printf(const char *, ...);
+""")
+libc = ffi.dlopen(None)   # standard library
+libc.printf("hello, %s!\n", ffi.new("char[]", ["world".encode()] ))
+
