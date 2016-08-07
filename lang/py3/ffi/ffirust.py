f352359358f093e1b00bb8b4d449f0cce7d5c29f
Zhou Mo <cdluminate@gmail.com>
2016-08-07 13:00:37 +0000

    lang:py3|rust:ffi: fix bug in ffirust.py and kernel.rs

--- lang/py3/ffi/ffirust.py
+++ lang/py3/ffi/ffirust.py
@@ -5,8 +5,28 @@ import ctypes
 
 kernel = ctypes.CDLL("./libkernel.so")
 
-''' without this line this program will end up with "Illegal Instruction" '''
-#print(kernel._ZN6kernel6kernel17h6daf402dcf741961E)
+try:
+  ''' without this line this program will end up with "Illegal Instruction" '''
+  print(kernel._ZN6kernel6kernel17h6daf402dcf741961E) # s: &'static str
+  msg = "Hello rust library".encode("ascii")
+  print(kernel._ZN6kernel6kernel17h6daf402dcf741961E(msg)) # this matches rust function definition but doesn't work
+  print(kernel._ZN6kernel6kernel17h6daf402dcf741961E(ctypes.c_char_p(u"hello".encode()), ctypes.c_size_t(5)))
+  print(kernel._ZN6kernel6kernel17h6daf402dcf741961E(ctypes.c_char_p(u"hello".encode("UTF-8")), 5))
+  print(kernel._ZN6kernel6kernel17h6daf402dcf741961E(u"hello".encode("UTF-8"), 5))
+  print(kernel._ZN6kernel6kernel17h6daf402dcf741961E(bytes(u"hello".encode("UTF-8")), 5))
+except:
+  print("_ZN6kernel6kernel17h6daf402dcf741961E not working")
 
-print(kernel._ZN6kernel6kernel17h6daf402dcf741961E("hello"))
+try:
+  print(kernel._ZN6kernel6kernel17h081d6f856694018aE) # s: &String
+  print(kernel._ZN6kernel6kernel17h081d6f856694018aE(ctypes.c_char_p(u"hello".encode())))
+  print(kernel._ZN6kernel6kernel17h081d6f856694018aE(u"hello".encode()))
+except:
+  print("_ZN6kernel6kernel17h081d6f856694018aE not working")
 
+try:
+  print(kernel._ZN6kernel6kernel17h48d014a057321023E) # s: &str
+  print(kernel._ZN6kernel6kernel17h48d014a057321023E(ctypes.c_char_p(u"hello".encode())))
+  print(kernel._ZN6kernel6kernel17h48d014a057321023E(ctypes.c_char_p(u"hello")))
+except:
+  print("_ZN6kernel6kernel17h48d014a057321023E not working")
--- lang/py3/ffi/kernel.rs
+++ lang/py3/ffi/kernel.rs
@@ -1,5 +1,21 @@
 
 pub fn kernel (s: &'static str) -> i32 {
-  println!("hello rust lib {}", s.to_string());
+//pub fn kernel (s: &'static str, size: &usize) -> i32 {
+//pub fn kernel (s: &String) -> i32 {
+//pub fn kernel (s: &str) -> i32 {
+  println!("arg string [{}] len [{}]", s, s.len());
+  println!("hello rust lib: {:?}", s);
+  unsafe{
+    // s.as_ptr() -> *const u8
+    let s_ptr = s.as_ptr();
+    //for i in 0..s.len() {
+    for i in 0..s.len() {
+      let x = s_ptr as usize + i;
+      let xp = x as *const u8;
+      print!("{}", *xp as char);
+    }
+    println!("");
+    println!("hello rust lib: {:?}", s);
+  }
   42
 }
