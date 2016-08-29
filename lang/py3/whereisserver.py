e466d5d33a5c563f81c83a16322b8e61bc01f8f8
Zhou Mo <cdluminate@gmail.com>
2016-08-29 09:34:11 +0000

    lang:py3: add whereisserver.py

---
+++ lang/py3/whereisserver.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+
+import pycurl
+import sys
+
+class Msg(object):
+  def __init__(self):
+    self.content = b''
+  def callback(self, buf):
+    self.content = self.content + buf
+
+def scan_curl(target):
+  msg = Msg()
+  curl = pycurl.Curl()
+  curl.setopt(pycurl.URL, bytes(target.encode()))
+  curl.setopt(pycurl.TIMEOUT_MS, 50)
+  curl.setopt(pycurl.WRITEFUNCTION, msg.callback)
+  try:
+    curl.perform()
+  except pycurl.error as e:
+    eid = e.args[0]
+    message = e.args[1]
+    if eid == 7: # Connection refused
+      return 0
+    elif eid == 28: # Connection timed out
+      return 0
+    else:
+      print(target, eid, message)
+      return 0
+  if len(msg.content) > 0:
+    print('Caught reply: {} : {}'.format(target, curl.getinfo(pycurl.HTTP_CODE)))
+    print(msg.content.decode())
+  sys.stdout.flush()
+  sys.stderr.flush()
+  return curl.getinfo(pycurl.HTTP_CODE)
+
+if __name__ == '__main__':
+  targets = []
+  for i in range(1, 256):
+    for j in range(1, 256):
+      targets.append('10.170.{}.{}:10022'.format(i,j))
+  print("Scanning {} hosts ...".format(len(targets)))
+  list(map(scan_curl, targets))
