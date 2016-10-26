7094004ee4f14a9c6a986216e01a2a44af664414
Zhou Mo <cdluminate@gmail.com>
2016-10-26 07:54:49 +0000

    lang:py3: update journallite

--- lang/py3/journallite.py
+++ lang/py3/journallite.py
@@ -15,29 +15,24 @@ with open('{}.recall'.format(sys.argv[1].strip()), 'w+') as f:
   f.write(str(out.decode()))
 print('I: wrote recall')
 
-# round 1: get bottom line number
+# round 1: get bottom 50K lines
 print('I: get bottom line number')
 with open(sys.argv[1].strip(), 'r') as f:
   line = f.readline(); k = 1 # small buffer requirement
+  linequeue = [ line ]
   while len(line)>0:
     #print(k, line.strip())
     if k%1000000==0: print('I: round1: read {} lines'.format(k))
     line = f.readline(); k = k + 1
+    linequeue.append(line)
+  if len(linequeue) > 500000:
+    del linequeue[:-500000]
   bottom_line_num = k
   print('I: found {} lines in total'.format(bottom_line_num))
 
-  # round 2: write lite version
-  print('I: write lite version')
-  f.seek(0)
+  # write lite version
+  print('I: write lite version with 50K lines')
   with open(sys.argv[1].strip()+'.lite', 'w+') as target:
-    line = f.readline(); k = 1
-    while len(line)>0:
-      if k%1000000==0: print('I: round2: read {} lines'.format(k))
-      if (k < int(bottom_line_num * 0.05)) or \
-         (k>int(bottom_line_num*0.48) and (k<int(bottom_line_num*0.52))) or \
-         (k > int(bottom_line_num * 0.95)):
-        #print(line.strip())
-        target.write(line)
-      line = f.readline(); k = k + 1
+    target.writelines(linequeue)
 
 print('I: done')
