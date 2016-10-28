4e287b16cd8c0eccadd9d28eefb1f0bf4ddba8ce
Zhou Mo <cdluminate@gmail.com>
2016-10-28 12:48:07 +0000

    lang:C: import simplest kernel module

---
+++ lang/c/kmodule/kmodule.c
@@ -0,0 +1,23 @@
+
+/* the simplest kernel module */
+
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/init.h>
+
+static int __init
+init_my_module (void) {
+  printk (KERN_INFO "Hello world!\n");
+  return 0;
+}
+
+static void __exit
+exit_my_module (void) {
+  printk (KERN_INFO "Bye world!\n");
+}
+
+module_init(init_my_module);
+module_exit(exit_my_module);
+
+MODULE_LICENSE("MIT");
+MODULE_AUTHOR("Anonymous");
