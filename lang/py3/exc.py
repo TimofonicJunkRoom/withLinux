d4689b4f85bea34d724555971dd947fba3188b00
Zhou Mo <cdluminate@gmail.com>
2016-12-12 03:44:19 +0000

    lang:py3: update exc.py

--- lang/py3/exc.py
+++ lang/py3/exc.py
@@ -5,3 +5,11 @@ import sys
 
 if len(sys.argv) < 2:
   raise Exception('missing argument')
+
+class BrainDeadException(Exception):
+  pass
+
+try:
+  raise BrainDeadException("oops")
+except Exception as e:
+  print(e)
