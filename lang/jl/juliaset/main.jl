077227a0eaf8256632afbd4ba5d303a230b155e4
Zhou Mo <cdluminate@gmail.com>
2016-12-16 15:46:24 +0000

    lang:jl: update juliaset

--- lang/jl/juliaset/main.jl
+++ lang/jl/juliaset/main.jl
@@ -1,22 +1,24 @@
+#!/usr/bin/julia
+
 # Calculate Julia Set with Julia.
-#
-# https://en.wikipedia.org/wiki/Julia_set      for "julia set"
-# http://mathworld.wolfram.com/JuliaSet.html   for "julia set"
-# https://en.wikipedia.org/wiki/Netpbm_format  for "ppm picture format"
 #
 # The resulting image will be saved in PPM format to file result.ppm
 # This ppm file can be viewed directly, or you can convert it into
 # another format like this:
-#  $ ffmpeg -i result.ppm -vcodec png output.png
+#   $ ffmpeg -i result.ppm -vcodec png output.png
 # This command provides performance information.
-#  $ sudo /usr/bin/time perf stat julia main.jl
+#   $ sudo /usr/bin/time perf stat julia main.jl
+#
+# https://en.wikipedia.org/wiki/Julia_set      for "julia set"
+# http://mathworld.wolfram.com/JuliaSet.html   for "julia set"
+# https://en.wikipedia.org/wiki/Netpbm_format  for "ppm picture format"
 # 
 # Copyright © Zhou Mo <cdluminate AT gmail.com>
 # MIT License
 
 # configure
-C = -0.62772 -0.42193im
-#C = 0.285 + 0im
+#C = -0.62772 -0.42193im
+C = 0.285 + 0im
 xmin = -1.8
 xmax = 1.8
 ymin = -1.8
@@ -30,9 +32,9 @@ samples = 2000
 ")
 
 # helper functions
-function getPPMHeader(width, height)
+function getPPMHeader(width, height, magic="P2")
    # Magic "P2" for gray PPM picture
-   return "P2
+   return "$(magic)
    $(width) $(height)
    255
    "
@@ -51,6 +53,29 @@ function normalize(mat)
    return (mat - minimum(mat)) / (maximum(mat) - minimum(mat))
 end
 
+function fakecolorize(mat)
+   I, J = size(mat)
+   ret = zeros(UInt8, I, J*3)
+   for i = 1:I
+      for j = 1:J
+         # ret[i, (j-1)*3+1:j*3] = mat[i,j]
+         needle = round(UInt8, mat[i,j])
+         value = [ 0, 0, 0 ]
+         if needle <= 63
+            value = [ 0, 4*needle, 255 ]
+         elseif needle <= 127
+            value = [ 0, 255, 511-4*needle ]
+         elseif needle <= 191
+            value = [ 4*needle-511, 255, 0 ]
+         else
+            value = [ 255, 1023- 4*needle, 0 ]
+         end
+         ret[i, (j-1)*3+1:j*3] = value
+      end
+   end
+   return ret
+end
+
 # calculate
 output = zeros(samples, samples)
 @printf("Calculating Julia Set\n")
@@ -66,10 +91,16 @@ end
 output = round(UInt8, 255*normalize(output))
 
 # save picture
-@printf("Saving result to result.ppm\n")
-f = open("result.ppm", "w+")
-write(f, getPPMHeader(samples, samples))
-writedlm(f, output, " ")
-close(f)
+#@printf("Saving gray result to result.gray.ppm\n")
+#f = open("result.gray.ppm", "w+")
+#write(f, getPPMHeader(samples, samples))
+#writedlm(f, output, " ")
+#close(f)
+
+@printf("Saving color result to result.color.ppm\n")
+fc = open("result.color.ppm", "w+")
+write(fc, getPPMHeader(samples, samples, "P3"))
+writedlm(fc, fakecolorize(output), " ")
+close(fc)
 
 @printf("Done\n")
