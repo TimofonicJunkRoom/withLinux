6a2b65ce853d01c55cac20651b63cd5557aaecf4
Zhou Mo <cdluminate@gmail.com>
2017-04-18 13:10:12 +0000

    lang:java: update

--- lang/java/rundemo.sh
+++ lang/java/rundemo.sh
@@ -16,5 +16,6 @@ java Main
 echo "making jar package"
 jar cvfm hello.jar MANIFEST.MF -C . .
 java -jar hello.jar
+# See maven for better project management
 
 # reference: http://docs.oracle.com/javase/tutorial/getStarted/cupojava/unix.html
