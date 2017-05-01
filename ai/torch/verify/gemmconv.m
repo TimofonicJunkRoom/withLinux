0c7fa57522005f1212595410c29b0cad18878859
Zhou Mo <cdluminate@gmail.com>
2017-05-01 06:01:58 +0000

    ai:verify: update

--- ai/torch/verify/gemmconv.m
+++ ai/torch/verify/gemmconv.m
@@ -1,5 +1,5 @@
 
-% regular 1d convolution
+% 1d 'full' convolution
 S = 0;
 vsize = 10;
 ksize = 3;
@@ -28,6 +28,36 @@ for i = 1:100
 
 	% reference
 	ref = conv(signal, k);
+
+	% comparison
+	S = S + sum(abs(res - ref));
+end
+S
+
+% 1d 'valid' convolution
+S = 0;
+vsize = 10;
+ksize = 3;
+tsize = vsize - ksize + 1;
+
+for i = 1:100
+	% populate signal
+	signal = rand(vsize, 1);
+
+	% populate kernel and trans mat
+	k = rand(ksize, 1);
+	krev = rot90(rot90(k));
+	trans = zeros(tsize, vsize);
+	for i = 1:tsize
+		trans(i,i:i+ksize-1) = krev(:);
+	end
+	trans;
+
+	% gemm
+	res = trans * signal;
+
+	% reference
+	ref = conv(signal, k, 'valid');
 
 	% comparison
 	S = S + sum(abs(res - ref));
