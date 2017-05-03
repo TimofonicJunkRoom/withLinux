c7c88984726b41af439dbcec87b3c9ec74cc5b87
Zhou Mo <cdluminate@gmail.com>
2017-05-03 11:00:34 +0000

    lang:go: update

--- lang/go/quick.go
+++ lang/go/quick.go
@@ -153,7 +153,116 @@ love:
 }
 
 func learnFunctionFactory() {
-
+	fmt.Println(sentenceFactory("summer")("A beautiful", "day"))
+	d := sentenceFactory("summer")
+	fmt.Println(d("A beautiful", "day"))
+	fmt.println(d("A lazy", "afternoon"))
 }
 
-TODO
+func sentenceFactory(mystring string) func(before, after string) string {
+	return func(before, after string) string {
+		return fmt.Sprintf("%s %s %s", before, mystring, after)
+	}
+}
+
+func learnDefer() (ok bool) {
+	defer fmt.Println("deferred statements execute in LIFO order")
+	defer fmt.Println("\nThis line is being printed first because")
+	return true
+}
+
+type Stringer interface {
+	String() string
+}
+
+type pair struct {
+	x, y int
+}
+
+func (p pair) String() string { // p is called the reciever
+	return fmt.Sprintf("%d %d", p.x, p.y)
+}
+
+func learnInterfaces() {
+	p := pair{3,4}
+	fmt.Println(p.String())
+	var i Stringer
+	i = p
+	fmt.Println(i.String())
+
+	fmt.Println(p)
+	fmt.Println(i)
+	learnVariadicParams("great", "learning", "here!")
+}
+
+func learnVariadicParams(myStrings ...interface{}) {
+	for _, param := range myStrings {
+		fmt.Println("param:", param)
+	}
+	fmt.Println("params:", fmt.Sprintln(myStrings...))
+
+	learnErrorHandling()
+}
+
+func learnErrorHandling() {
+	m := map[int]string{3: "three", 4: "four"}
+
+	if x, ok := m[1]; !ok { // "; ok" idiom
+		fmt.Println("no one there")
+	} else {
+		fmt.Println(x)
+	}
+
+	if _, err := strconv.Atoi("non-int"); err != nil {
+		fmt.Println(err)
+	}
+	learnConcurrency()
+}
+
+func inc(i int, c chan int) {
+	c <- i + 1 // <- is the "send" operator when a channel appears on the left
+}
+
+func learnConcurrency() {
+	c := make(chan int)
+	go inc(0, c) // "go" starts a new goroutine
+	go inc(10, c)
+	go inc(-805, c)
+	fmt.Println(<-c, <-c, <-c) // <- is the receive operator
+
+	cs := make(chan string)
+	ccs := make(chan chan string)
+	go func() { c <- 84 }()
+	go func() { cs <- "wordy" }()
+	select {
+	case i := <-c:
+		fmt.Printf("it's a %T", i)
+	case <- cs:
+		fmt.Println("it's a string")
+	case <- ccs:
+		fmt.Println("didn't happen.")
+	}
+
+	learnWebProgramming()
+}
+
+func learnWebProgramming() {
+	go func() {
+		err := http.ListenAndServe(":8080", pair{})
+		fmg.Println(err)
+	}()
+
+	requestServer()
+}
+
+func (p pair) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("You learned Go in Y minutes"))
+}
+
+func requestServer() {
+	resp, err := http.Get("http://localhost:8080")
+	fmt.Println(err)
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	fmt.Printf("\nWebserver said: %s", string(body))
+}
