541b8265fa9e8b3c0350ac907ff2f719a24e7d97
Zhou Mo <cdluminate@gmail.com>
2017-05-20 08:35:33 +0000

    dotfile: update fish config

--- dotfile/config.fish
+++ dotfile/config.fish
@@ -7,3 +7,19 @@ end
 
 set -g theme_powerline_fonts no
 set -g theme_display_user yes
+
+# start debian packaging block
+
+function dquilt
+	quilt --quiltrc=$HOME/.quiltrc-dpkg $argv
+end
+
+export DEBEMAIL="cdluminate@gmail.com"
+export DEBFULLNAME="Zhou Mo"
+export DEB_BUILD_OPTIONS=parallel=4
+export BUILDER=pbuilder
+
+# end debian packaging block
+
+export GIT_EDITOR=vim
+
