24bab3ebe7fc33b1b4c287b43a31b4db8d7d36ee
Zhou Mo <cdluminate@gmail.com>
2017-05-22 03:53:31 +0000

    dotfile: update fish config

--- dotfile/config.fish
+++ dotfile/config.fish
@@ -5,6 +5,10 @@ function jl
 	/home/lumin/Downloads/julia-6445c82d00/bin/julia $argv
 end
 
+function gitpushall
+	git push; git push --all; git push --tags
+end
+
 set -g theme_powerline_fonts no
 set -g theme_display_user yes
 
