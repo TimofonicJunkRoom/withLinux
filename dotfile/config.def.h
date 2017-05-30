020c9a7c5fdedd8d444df4d3427d8786491bfd52
Zhou Mo <cdluminate@gmail.com>
2017-05-30 06:00:44 +0000

    dotfile: update dwm

--- dotfile/config.def.h
+++ dotfile/config.def.h
@@ -38,6 +38,8 @@ static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+//
+static const char dmenupromptstr[]  = "dwm ➤";
 
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
@@ -87,7 +89,9 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbgcolor, "-sf", selfgcolor, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon,
+   	"-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor,
+   	"-sb", selbgcolor, "-sf", selfgcolor, "-p", dmenupromptstr, NULL };
 static const char *termcmd[]  = { "sakura", NULL };
 static const char *lockcmd[]  = { "slock", NULL };
 static const char *cmdalv[]   = { "amixer", "-q", "sset", "Master", "5%-", NULL };
