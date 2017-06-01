d1b5c9a5c79dad1906c7050025f2e0bb16fa7698
Zhou Mo <cdluminate@gmail.com>
2017-06-01 01:59:02 +0000

    dotfile: update

--- dotfile/dwm.mk
+++ dotfile/dwm.mk
@@ -1,9 +1,18 @@
 dwm:
 	keyrate || xset r rate 170 96 # dwm keyrate
-	feh --bg-scale $(BGIMG) || xsetroot -solid black # dwm background
+	feh --bg-scale $(BGIMG) || xsetroot -solid gray # dwm background
+
+	-pkill -f -u $$USER '.*/bin/sh.*-c.*while true.*dwmstatus.*done'
 	while true; do dwmstatus; done & # dwm status bar
-	killall dunst; dunst & # dwm notification
-	killall xbindkeys; xbindkeys  # dwm shortcuts
-	killall clipmenud; clipmenud & # dwm clipboard
-	#xcompmgr & # transparent bar patch
-	ps aux | grep '.*sh.*while true'
+
+	-pkill -u $$USER dunst;
+	dunst & # dwm notification
+
+	-pkill -u $$USER xbindkeys;
+	xbindkeys  # dwm shortcuts
+
+	-pkill -u $$USER clipmenud;
+	clipmenud & # dwm clipboard
+
+	-pkill -u $$USER xcompmgr;
+	xcompmgr & # transparent bar patch
