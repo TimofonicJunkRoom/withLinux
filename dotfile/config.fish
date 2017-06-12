c8dcb680141c90f15908fca4576d38f8b6aeeb79
Zhou Mo <cdluminate@gmail.com>
2017-06-12 12:38:36 +0000

    dotfile: update fish config

--- dotfile/config.fish
+++ dotfile/config.fish
@@ -5,6 +5,10 @@ function jl
 	/home/lumin/Downloads/julia-6445c82d00/bin/julia $argv
 end
 
+function ips
+	ip -s -c -h a
+end
+
 function gitpushall
 	git push; git push --all; git push --tags
 end
