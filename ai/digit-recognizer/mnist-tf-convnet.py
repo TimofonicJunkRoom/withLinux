6781affe20af56b74df4db23610be7c6e189c58c
Zhou Mo <cdluminate@gmail.com>
2017-06-29 09:32:38 +0000

    ai: grooming

--- ai/digit-recognizer/mnist-tf-convnet.py
+++ ai/digit-recognizer/mnist-tf-convnet.py
@@ -76,17 +76,6 @@ train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
 correct_prediction = tf.equal(tf.argmax(y_,1), tf.argmax(y,1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
 
-#for i in range(20000):
-#    batch = mnist.train.next_batch(50)
-#    if i%100 == 0:
-#        train_accuracy = accuracy.eval(feed_dict={
-#            x:batch[0], y_: batch[1], keep_prob: 1.0})
-#        print "step %d, training accuracy %g"%(i, train_accuracy)
-#    train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
-#
-#print "test accuracy %g"%accuracy.eval(feed_dict={
-#    x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0})
-
 ### Train and Val ###
 sess = tf.Session()
 sess.run(tf.global_variables_initializer())
@@ -99,6 +88,7 @@ train_writer = tf.summary.FileWriter("mnist-convnet/train-{}".format(profilename
 test_writer  = tf.summary.FileWriter("mnist-convnet/test-{}".format(profilename))
 
 for i in range(20000):
+# NOTE:
 # could not set cudnn tensor descriptor: CUDNN_STATUS_BAD_PARAM
 # batchsize 0 will cause the above zero, be careful when reading.
 # when your index for reading data is wrong, the batch could be 0.
