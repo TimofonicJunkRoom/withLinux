7bcc51d76ed5f8d1062112578489ebec060059bd
Zhou Mo <cdluminate@gmail.com>
2017-07-06 16:05:50 +0000

    ai: update script

--- ai/digit-recognizer/mnist-tf-convnet.py
+++ ai/digit-recognizer/mnist-tf-convnet.py
@@ -24,51 +24,35 @@ test_images = test.iloc[:,:].div(255)
 print('-> test  set shape', test_images.shape)
 
 ### Setup Graph ###
-x = tf.placeholder(tf.float32, [None, 784], name='x')
-y = tf.placeholder(tf.float32, [None, 10], name='y')
-
-def weight_variable(shape):
-    initial = tf.truncated_normal(shape, stddev=0.1)
-    return tf.Variable(initial)
-
-def bias_variable(shape):
-    initial = tf.constant(0.1, shape=shape)
-    return tf.Variable(initial)
-
-def conv2d(x, W):
-    return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
-
-def max_pool_2x2(x):
-    return tf.nn.max_pool(x, ksize=[1, 2, 2, 1],
-                              strides=[1, 2, 2, 1], padding='SAME')
+x = tf.placeholder(tf.float32, [None, 784], name='x')         # <-- Placeholder
+y = tf.placeholder(tf.float32, [None, 10], name='y')          # <-- Placeholder
 
 x_image = tf.reshape(x, [-1,28,28,1]) # localid, width, height, channel
 
-W_conv1 = weight_variable([5, 5, 1, 32])
-b_conv1 = bias_variable([32])
+W_conv1 = tf.Variable(tf.truncated_normal([5, 5, 1, 32], stddev=0.1))
+b_conv1 = tf.Variable(tf.constant(0.1, shape=[32]))
+h_conv1 = tf.nn.relu(tf.nn.conv2d(
+    x_image, W_conv1, strides=[1, 1, 1, 1], padding='SAME') + b_conv1)
+h_pool1 = tf.nn.max_pool(h_conv1, ksize=[1, 2, 2, 1],
+    strides=[1, 2, 2, 1], padding='SAME')
 
-h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
-h_pool1 = max_pool_2x2(h_conv1)
+W_conv2 = tf.Variable(tf.truncated_normal([5, 5, 32, 64], stddev=0.1))
+b_conv2 = tf.Variable(tf.constant(0.1, shape=[64]))
+h_conv2 = tf.nn.relu(tf.nn.conv2d(
+    h_pool1, W_conv2, strides=[1, 1, 1, 1], padding='SAME') + b_conv2)
+h_pool2 = tf.nn.max_pool(h_conv2, ksize=[1, 2, 2, 1],
+    strides=[1, 2, 2, 1], padding='SAME')
 
-W_conv2 = weight_variable([5, 5, 32, 64])
-b_conv2 = bias_variable([64])
-
-h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
-h_pool2 = max_pool_2x2(h_conv2)
-
-W_fc1 = weight_variable([7 * 7 * 64, 1024])
-b_fc1 = bias_variable([1024])
-
+W_fc1 = tf.Variable(tf.truncated_normal([7 * 7 * 64, 1024], stddev=0.1))
+b_fc1 = tf.Variable(tf.constant(0.1, shape=[1024]))
 h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*64])
 h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
-
-keep_prob = tf.placeholder(tf.float32, name='keep_prob')
+keep_prob = tf.placeholder(tf.float32, name='keep_prob')      # <-- Placeholder
 h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
 
-W_fc2 = weight_variable([1024, 10])
-b_fc2 = bias_variable([10])
-
-y_ =tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2)
+W_fc2 = tf.Variable(tf.truncated_normal([1024, 10], stddev=0.1))
+b_fc2 = tf.Variable(tf.constant(0.1, shape=[10]))
+y_ = tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2)
 
 cross_entropy = -tf.reduce_sum(y*tf.log(y_))
 
@@ -103,7 +87,7 @@ for i in range(20000):
             tfsmmall , train_step],
             feed_dict={x: batch_images, y: batch_labels, keep_prob: 0.5})
     train_writer.add_summary(summary, i)
-    if i % 20 == 0:
+    if i % 5 == 0:
         #print('-> data range {} - {}'.format( (i*50)%33600,
         #    (i+1)%672==0 and 33600 or ((i+1)*50)%33600))
         testacc, summary = sess.run([accuracy, tfsmmall ],
