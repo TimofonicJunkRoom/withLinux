99ffa9db168ee39351a2d8923a8b9059309792ed
Zhou Mo <cdluminate@gmail.com>
2017-07-27 15:49:20 +0000

    ai: update

--- ai/cifar10/cifar10-th-convnet.py
+++ ai/cifar10/cifar10-th-convnet.py
@@ -6,6 +6,7 @@ import torch.nn.functional as F
 from torch.autograd import Variable
 import numpy as np
 from dataloader import DataLoader
+from collections import OrderedDict
 
 perf_tm = {}
 perf_getdiff = lambda d: d['end'] - d['start']
@@ -15,7 +16,8 @@ os.putenv('OPENBLAS_NUM_THREADS', X_THNUM)
 os.putenv('OMP_NUM_THREADS', X_THNUM)
 os.putenv('MKL_NUM_THREADS', X_THNUM)
 
-USE_GPU = True if len(sys.argv)>1 else False # Append any argument to command line to toggle GPU mode
+USE_GPU = False if len(sys.argv)>1 else True
+X_MAXITER = 5000
 
 print('-> Using TH', th.__version__)
 print('-> USE_GPU: {}'.format(USE_GPU))
@@ -31,55 +33,39 @@ dataloader = DataLoader()
 
 ### Model ###
 class QuickNet(th.nn.Module):
-    ''' Reference: caffe/examples/cifar10
-    data (-1,3,32,32) -> conv1 (-1,32,32,32) ->
-    pool1 (-1,32,16,16) -> relu1 (.) ->
-    conv2 (-1,32,16,16) -> relu2 (.) ->
-    pool2 (-1,32,8,8) -> conv3 (-1,64,8,8) ->
-    relu3 (.) -> pool3 (-1,64,4,4) (1024) ->
-    ip1 (-1, 64) -> ip2 (-1,10) ->
-    softmaxwithloss
+    ''' Reference: caffe/examples/cifar10 # 70%
+    https://github.com/tensorflow/models/blob/master/tutorials/image/cifar10/cifar10.py
     '''
     def __init__(self):
         super(QuickNet, self).__init__()
-        self.conv1 = th.nn.Conv2d(3, 32, 5, stride=1, padding=2)
-        self.conv2 = th.nn.Conv2d(32, 32, 5, stride=1, padding=2)
-        self.conv3 = th.nn.Conv2d(32, 64, 5, stride=1, padding=2)
-        self.fc1 = th.nn.Linear(1024, 64)
-        self.fc2 = th.nn.Linear(64, 10)
+        self.SEQ1 = th.nn.Sequential(OrderedDict([
+('conv1', th.nn.Conv2d(3, 32, 5, stride=1, padding=2)), #64,32,32
+('relu1', th.nn.ReLU()),
+('pool1', th.nn.MaxPool2d(3, stride=2, padding=1)), #64,16,16
+('bn1',   th.nn.BatchNorm2d(32)),
+('conv2', th.nn.Conv2d(32, 64, 5, stride=1, padding=2)), #64,16,16
+('relu2', th.nn.ReLU()),
+('bn2',   th.nn.BatchNorm2d(64)),
+('pool2', th.nn.MaxPool2d(3, stride=2, padding=1)), #64,8,8
+]))
+        self.SEQ2 = th.nn.Sequential(OrderedDict([
+('fc4',   th.nn.Linear(4096, 384)),
+('relu4', th.nn.ReLU()),
+('bn4',   th.nn.BatchNorm1d(384)),
+('fc5',   th.nn.Linear(384, 192)),
+('bn6',   th.nn.BatchNorm1d(192)),
+('fc6',   th.nn.Linear(192, 10)),
+]))
     def forward(self, x):
-        #q = lambda msg: print(msg, x.size())
-        q = lambda msg: True
-        q('data')
-        x = self.conv1(x) # 3,32,32 -> 32,32,32
-        q('conv1')
-        x = F.max_pool2d(x, 3, stride=2, padding=1) # 32,16,16
-        q('pool1')
-        x = F.relu(x)
-        q('relu')
-        x = self.conv2(x) # 32,16,16
-        q('conv2')
-        x = F.relu(x)
-        q('relu')
-        x = F.max_pool2d(x, 3, stride=2, padding=1) # 32,8,8
-        q('pool2')
-        x = self.conv3(x) # 64,8,8
-        q('conv3')
-        x = F.relu(x)
-        q('relu')
-        x = F.max_pool2d(x, 3, stride=2, padding=1) # 64,4,4
-        q('pool')
-        x = x.view(-1, 1024)
-        x = self.fc1(x)
-        q('fc1')
-        x = self.fc2(x)
-        q('fc2')
+        x = self.SEQ1(x)
+        x = x.view(-1, 4096)
+        x = self.SEQ2(x)
         return x
 
 net = QuickNet() if not USE_GPU else QuickNet().cuda()
 print(net)
 crit = th.nn.CrossEntropyLoss()
-optimizer = th.optim.Adam(net.parameters(), lr=1e-2)
+optimizer = th.optim.Adam(net.parameters(), lr=1e-3)
 
 ### Train
 def transform(images, labels):
@@ -89,18 +75,17 @@ def transform(images, labels):
     if USE_GPU: images, labels = images.cuda(), labels.cuda() 
     return images, labels
 
-#for i in range(100+1):
 perf_tm['start'] = time.time()
-for i in range(500+1):
+for i in range(X_MAXITER+1):
     # read data
     images, labels = dataloader.getBatch('trainval', 100)
     images, labels = transform(images, labels)
 
     # half the learning rate @ iter 500
-    if i==500 or i==1000 or i==2000:
-        print('-> half the learning rate')
+    if i!=0 and i%1000==0:
+        print('-> *0.7 the learning rate')
         for param_group in optimizer.param_groups:
-            param_group['lr'] *= 0.5
+            param_group['lr'] *= 0.7
             print(param_group['lr'])
 
     # train
@@ -138,7 +123,6 @@ for i in range(500+1):
         print('-> TEST @ {} |'.format(i),
                 'Loss {:7.3f} |'.format(lossaccum),
                 'Accu {:.5f}|'.format(correct / total))
-
 
 perf_tm['end'] = time.time()
 print('-> done, time elapsed', perf_getdiff(perf_tm))
