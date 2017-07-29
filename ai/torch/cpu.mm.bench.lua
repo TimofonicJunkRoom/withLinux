79c69b02340d3fe0e46215ff575fb13f4de3266e
Zhou Mo <cdluminate@gmail.com>
2017-07-29 13:29:20 +0000

    ai: import snippet

---
+++ ai/torch/cpu.mm.bench.lua
@@ -0,0 +1,41 @@
+
+require 'torch'
+
+local max=600
+
+print('begin double')
+torch.setdefaulttensortype('torch.DoubleTensor')
+local tm = torch.Timer()
+for i = 1,max do
+	local a = torch.rand(i, i)
+	local b = torch.rand(i, i)
+	local c = torch.mm(a, b)
+end
+tm:stop()
+print('done', tm:time())
+
+print('begin float')
+torch.setdefaulttensortype('torch.FloatTensor')
+local tm = torch.Timer()
+for i = 1,max do
+	local a = torch.rand(i, i)
+	local b = torch.rand(i, i)
+	local c = torch.mm(a, b)
+end
+tm:stop()
+print('done', tm:time())
+
+--[[ without setting OMP_NUM_THREADS / OMP_NUM_THREADS=4
+
+begin double  2520M
+  real : 6.1052939891815
+begin float	
+  real : 4.8923819065094
+
+    OMP_NUM_THREADS=2 th cpubench.lua 
+
+begin double	2520M
+  real : 3.8089778423309
+begin float	
+  real : 2.7467200756073
+--]]
