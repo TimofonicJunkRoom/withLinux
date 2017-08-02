a10a4e60f87bd23233558f07667367ee121a6faf
Zhou Mo <cdluminate@gmail.com>
2017-08-02 01:40:18 +0000

    lang:py3: update

--- lang/py3/imageset.py
+++ lang/py3/imageset.py
@@ -18,7 +18,7 @@ https://caffe2.ai/docs/tutorial-image-pre-processing.html
 http://pytorch.org/tutorials/beginner/data_loading_tutorial.html
 '''
 
-# Parse command line
+## Parse command line
 parser = argparse.ArgumentParser()
 parser.add_argument('-i', type=str, action='store', dest='input',
                     required=True, help='image-label list file (train)')
@@ -38,44 +38,46 @@ parser.add_argument('-f', action='store_true', dest='force',
                     default=False, help='force overwrite output hdf5')
 args = parser.parse_args()
 
-# Read image list
-# -> list[ list[ str(path), str(label) ] ]
-imagelist = [l.strip().split() for l in open(args.input, 'r').readlines() ]
+## Configure
+compargs = {'compression':'gzip', 'compression_opts':1} if args.c else {}
+
+## Helpers
+def readlist(_fpath):
+    # -> list[ list[ str(path), str(label) ] ]
+    return [l.strip().split() for l in open(_fpath, 'r').readlines() ]
+
+def fillhdf5(_h5, _list, _group):
+    for i, line in enumerate(_list, 1):
+        if i%1000==0: print(' *> processed {} images'.format(i))
+        path, label = line
+        if i < 10: print(repr(path), repr(label))
+        image = Image.open(path).resize((args.pixels, args.pixels), Image.BILINEAR)
+        image = image.convert('RGB') # RGBA/... -> RGB
+        if i < 10: print('\t', image)
+        # image -> [0,255], H,W,C
+        image = np.asarray(image) # Image -> Numpy
+        # HWC -> CHW
+        image = image.swapaxes(0,2) # or image.transpose((1,2,0))
+        _h5[_group+'/images'][i-1,:,:,:] = image
+        _h5[_group+'/labels'][i-1,:] = int(label)
+
+# Read list files
+imagelist = readlist(args.input)
 if args.s: random.shuffle(imagelist)
-print('-> Found {} images'.format(len(imagelist)))
+print('-> Found {} images for training'.format(len(imagelist)))
 
 # Create output file
 if os.path.exists(args.output):
     if not args.force: raise SystemExit('HDF5 file {} already exists!'.format(args.output))
 h5 = h5py.File(args.output, 'w')
-if args.c:
-    h5.create_dataset('train/images',
-        (len(imagelist), 3, args.pixels, args.pixels), # N,C,H,W
-        dtype=np.byte, compression='gzip', compression_opts=1)
-    h5.create_dataset('train/labels',
-        (len(imagelist), 1), # N,1
-        dtype=np.int, compression='gzip', compression_opts=1)
-else:
-    h5.create_dataset('train/images',
-        (len(imagelist), 3, args.pixels, args.pixels), # N,C,H,W
-        dtype=np.byte)
-    h5.create_dataset('train/labels',
-        (len(imagelist), 1), dtype=np.int)
 
-# Traverse the image list
-for i, line in enumerate(imagelist, 1):
-    if i%1000==0: print(' *> processed {} images'.format(i))
-    path, label = line
-    if i < 10: print(repr(path), repr(label))
-    image = Image.open(path).resize((args.pixels, args.pixels), Image.BILINEAR).convert('RGB')
-    if i < 10: print('\t', image)
-    # image -> [0,255], H,W,C
-    image = np.asarray(image) # Image -> Numpy
-    # HWC -> CHW
-    image = image.swapaxes(0,2) # or image.transpose((1,2,0))
-    h5['train/images'][i-1,:,:,:] = image
-    h5['train/labels'][i-1,:] = int(label)
-print(' *> processed {} images in total'.format(len(imagelist)))
+# Fill HDF5
+h5.create_dataset('train/images', # N x C x H x W
+    (len(imagelist), 3, args.pixels, args.pixels), dtype=np.byte, **compargs)
+h5.create_dataset('train/labels', # N x 1
+    (len(imagelist), 1), dtype=np.int, **compargs)
+fillhdf5(h5, imagelist, 'train')
+print(' *> processed {} images'.format(len(imagelist)))
 
 # Write to disk
 h5.close()
