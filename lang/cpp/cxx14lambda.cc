f529ad4f453a752f864d840524e367d9ee052098
Mo Zhou <cdluminate@gmail.com>
2017-09-02 05:29:28 +0000

    lang:cxx: update

--- lang/cpp/cxx14lambda.cc
+++ lang/cpp/cxx14lambda.cc
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <functional>
 
 // https://msdn.microsoft.com/zh-cn/library/dd293608.aspx
 
@@ -8,5 +9,10 @@ main(void)
 	auto lambda = [](auto a, auto b) { return a+b; };
 	std::cout << lambda(1,2) << std::endl;
 	std::cout << lambda(3.5, 4.5) << std::endl;
+
+	std::function<long(int)> factorial = [&](int n) {
+		return (n==1) ? 1 : n*factorial(n-1);
+	};
+	std::cout << factorial(5) << std::endl;
 	return 0;
 }
