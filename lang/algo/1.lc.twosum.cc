5fe6b4549c26133b06cacd565e259676484c157c
Mo Zhou <cdluminate@gmail.com>
2017-10-12 07:02:27 +0000

    lang:algo: merge

--- lang/algo/1.lc.twosum.cc
+++ lang/algo/1.lc.twosum.cc
@@ -45,3 +45,23 @@ main(void)
 	cout << s.twoSum(v, 6) << endl;
 	return 0;
 }
+
+/* Time limite succeed
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        for (int i = 0; i < nums.size(); i++) {
+            for (int j = 0; j < nums.size(); j++) {
+                if (i == j) {
+                    continue;
+                } else {
+                    if (nums.at(i)+nums.at(j) == target) {
+                        return vector<int> {i, j};
+                    }
+                }
+            }
+        }
+        return vector<int> {-1, -1};
+    }
+};
+*/
