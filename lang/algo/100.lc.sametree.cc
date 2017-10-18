e9e86c792c5e6d38536391b70fd3bdd1acc76b4f
Mo Zhou <cdluminate@gmail.com>
2017-10-18 16:02:46 +0000

    lang:algo: update

--- lang/algo/100.lc.sametree.cc
+++ lang/algo/100.lc.sametree.cc
@@ -12,6 +12,8 @@ public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
         if (nullptr == p && nullptr == q) return true;
         if (nullptr == p || nullptr == q) return false;
-        return (p->val==q->val) && isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+        return (p->val==q->val) &&
+		   isSameTree(p->left, q->left) &&
+		   isSameTree(p->right, q->right);
     }
 };
