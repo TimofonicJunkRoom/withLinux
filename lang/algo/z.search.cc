95a4fdcab7c9a97f31f06bf07198a4f764c05074
Mo Zhou <cdluminate@gmail.com>
2017-10-25 13:36:48 +0000

    lang:algo: update

--- lang/algo/z.search.cc
+++ lang/algo/z.search.cc
@@ -1,5 +1,6 @@
-#include <vector>
 #include <iostream>
+#include <vector>
+#include "helper.hpp"
 using namespace std;
 
 bool bisearch_iter(vector<int>& v, int target)
@@ -46,9 +47,22 @@ bool bisearch_recu(vector<int>& v, int target) {
 	return bisearch_recu_(v, target, 0, v.size()-1);
 }
 
+/**
+ * @brief sequential search
+ */
+template <typename DType>
+int
+sequentialSearch(const std::vector<DType>& v, DType target)
+{
+	for (int i = 0; i < v.size(); i++)
+		if (v[i] == target) return i;
+	return -1;
+}
+
 int
 main(void)
 {
+	{
 	vector<int> v {1,2,3,4,5,6,7,8,9,10};
 	cout << bisearch_iter(v, -1) << bisearch_recu(v,-1) << endl;
 	cout << bisearch_iter(v, 1)  << bisearch_recu(v,1)  << endl;
@@ -56,5 +70,14 @@ main(void)
 	cout << bisearch_iter(v, 6)  << bisearch_recu(v,6)  << endl;
 	cout << bisearch_iter(v, 10) << bisearch_recu(v,10) << endl;
 	cout << bisearch_iter(v, 11) << bisearch_recu(v,11) << endl;
+	}
+
+	{
+	//int i;
+	//while (std::cin >> i) buf.push_back(i);
+	std::vector<int> buf {1,2,3,4,5,6,7,8,9};
+	std::cout << sequentialSearch(buf, 5) << std::endl;
+	std::cout << sequentialSearch(buf, 10) << std::endl;
+	}
 	return 0;
 }
