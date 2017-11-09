efa94ebef434f3484052def2e40f9a411da46ea2
Mo Zhou <cdluminate@gmail.com>
2017-11-09 06:58:37 +0000

    ai: update

--- ai/lite/tensor.cc
+++ ai/lite/tensor.cc
@@ -13,10 +13,57 @@ public:
 	std::vector<size_t> shape;
 	Dtype* data;
 
+	// 1D (vector) constructor
+	Tensor(size_t length) {
+		this->shape.push_back(length);
+		this->initMem();
+	}
+
+
+	// 2D (matrix) constructor
+	Tensor(size_t row, size_t col) {
+		this->shape.push_back(row);
+		this->shape.push_back(col);
+		this->initMem();
+	}
+
+	// 1D data pointer
+	Dtype* at(size_t row, size_t col) {
+		return this->data + row*this->shape[1] + col;
+	}
+
+	// 2D data pointer
+	Dtype* at(size_t offset) {
+		return this->data + offset;
+	}
+
+	// common dump
+	void dump() {
+		if (shape.size() == 1) {
+			std::cout << "[";
+			for (int i = 0; i < this->getSize(0); i++)
+				printf(" %.3f", *this->at(i));
+			std::cout << " ]" << std::endl;
+		} else if (shape.size() == 2) {
+			std::cout << "[" << std::endl;;
+			for (int i = 0; i < this->getSize(0); i++) {
+				std::cout << "  [";
+				for (int j = 0; j < this->getSize(1); j++) {
+					printf(" %.3f", *this->at(i, j));
+				}
+				std::cout << " ]" << std::endl;
+
+			}
+			std::cout << "]" << std::endl;
+		}
+	}
+
+	// common get dimension
 	size_t getDim() const {
 		return shape.size();
 	}
 
+	// common get size
 	size_t getSize() const {
 		if (shape.empty()) return 0;
 		size_t size = 1;
@@ -24,71 +71,32 @@ public:
 		return size;
 	}
 
+	// common get size
 	size_t getSize(int i) const {
 		return (i >= shape.size()) ? -1 : shape[i];
 	}
 
+	// common init
 	size_t initMem() {
 		data = (Dtype*)malloc(sizeof(Dtype)*getSize());
 		memset(data, 0x0, sizeof(Dtype)*getSize());
 	}
 
-	void zero_() { // inplace
+	// common inplace zero
+	void zero_() {
 		memset(data, 0x0, sizeof(Dtype)*getSize());
 	}
-};
 
-template <typename Dtype>
-class Vector : public Tensor<Dtype> {
-public:
-	Vector(size_t length) {
-		this->shape.push_back(length);
-		this->initMem();
-	}
-
-	Dtype* at(size_t offset) {
-		return this->data + offset;
-	}
-
-	void dump() {
-		std::cout << "[";
-		for (int i = 0; i < this->getSize(0); i++)
-			printf(" %.3f", *this->at(i));
-		std::cout << " ]" << std::endl;
-	}
-};
-
-template <typename Dtype>
-class Matrix : public Tensor<Dtype> {
-public:
-	Matrix(size_t row, size_t col) {
-		this->shape.push_back(row);
-		this->shape.push_back(col);
-		this->initMem();
-	}
-
-	Dtype* at(size_t row, size_t col) {
-		return this->data + row*this->shape[1] + col;
-	}
-
-	void dump() {
-		std::cout << "[" << std::endl;;
-		for (int i = 0; i < this->getSize(0); i++) {
-			std::cout << "  [";
-			for (int j = 0; j < this->getSize(1); j++) {
-				printf(" %.3f", *this->at(i, j));
-			}
-			std::cout << " ]" << std::endl;
-
-		}
-		std::cout << "]" << std::endl;
+	// common destructor
+	~Tensor() {
+		free(this->data);
 	}
 };
 
 template <typename Dtype>
 void
-GEMM(Dtype alpha, Matrix<Dtype>* A, Dtype beta, Matrix<Dtype>* B,
-		Dtype gamma, Matrix<Dtype>* C)
+GEMM(Dtype alpha, Tensor<Dtype>* A, Dtype beta, Tensor<Dtype>* B,
+		Dtype gamma, Tensor<Dtype>* C)
 {
 	// check shape
 	assert(A->shape[1] == B->shape[0]);
@@ -109,8 +117,8 @@ GEMM(Dtype alpha, Matrix<Dtype>* A, Dtype beta, Matrix<Dtype>* B,
 int
 main(void)
 {
-	Matrix<double> data (8, 17);
-	Vector<double> label (10);
+	Tensor<double> data (8, 17);
+	Tensor<double> label (10);
 
 	lite_hdf5_read("demo.h5", "data", 0, 0, 8, 17, data.data);
 	data.dump();
